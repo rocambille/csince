e9ee98587454322ca741fedfcb398764e8d9431c
Romain Guillemot <contact@romainguillemot.com>
2016-12-28 11:50:29 +0100

    Used textContent instead of innerHTML for security and performance, moved the added content after the title, made some cleaning

--- csince.cs.js
+++ csince.cs.js
@@ -1,3 +1,11 @@
+/* Copyright (C) 2016 Romain Guillemot
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+
 var cmake_versions = [
     "v3.0",
     "v3.1",
@@ -31,15 +39,20 @@ var h1_elements = document.getElementsByTagName('h1');
 var i;
 var checking_required = false;
 
-for (i = 0; i < h1_elements.length; ++i) {
-    if (h1_elements[i].innerHTML != 'Not Found') {
+for (i = 0; i < h1_elements.length; ++i)
+{
+    if (h1_elements[i].textContent != 'Not Found')
+    {
         checking_required = true;
 
         var csince_element = document.createElement('span');
         csince_element.className = 'csince';
-        csince_element.innerHTML = '(CSince checking version...)';
+        csince_element.textContent = '(CSince checking version...)';
 
-        h1_elements[i].appendChild(csince_element);
+        h1_elements[i].parentNode.insertBefore(
+            csince_element,
+            h1_elements[i].nextSibling
+        );
     }
 }
 
@@ -52,7 +65,8 @@ if (checking_required) {
 
     var initial_version_index = cmake_versions.indexOf(initial_version);
 
-    if (initial_version_index < 0) {
+    if (initial_version_index < 0)
+    {
         // use latest version
         initial_version_index = cmake_versions.length - 1;
     }
@@ -65,12 +79,24 @@ if (checking_required) {
     );
 }
 
-function check_version(checked_url_, lower_version_index_, upper_version_index_, checked_path_) {
-    if (lower_version_index_ > upper_version_index_) {
-        if (lower_version_index_ == 0) {
+function check_version(
+    checked_url_,
+    lower_version_index_,
+    upper_version_index_,
+    checked_path_
+)
+{
+    if (lower_version_index_ > upper_version_index_)
+    {
+        if (lower_version_index_ == 0)
+        {
             // reached v3.0: feature may be present in old versions
-            var checked_feature = checked_path.substr(checked_path.lastIndexOf('/') + 1);
-            checked_feature = checked_feature.substr(0, checked_feature.indexOf('.html'));
+            var checked_feature = checked_path.substr(
+                checked_path.lastIndexOf('/') + 1
+            );
+            checked_feature = checked_feature.substr(
+                0, checked_feature.indexOf('.html')
+            );
 
             check_old_version(
                 checked_url,
@@ -79,20 +105,29 @@ function check_version(checked_url_, lower_version_index_, upper_version_index_,
                 checked_feature
             );
         }
-        else {
+        else
+        {
             show_since(cmake_versions[lower_version_index_]);
         }
     }
-    else {
-        var current_version_index = Math.floor(lower_version_index_ + (upper_version_index_ - lower_version_index_)/2);
+    else
+    {
+        var current_version_index = Math.floor(
+            lower_version_index_ + (
+                upper_version_index_ - lower_version_index_
+            )/2
+        );
 
         var checked_version = cmake_versions[current_version_index];
 
         var xhttp = new XMLHttpRequest();
 
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4) {
-                if (this.status == 200) {
+        xhttp.onreadystatechange = function()
+        {
+            if (this.readyState == 4)
+            {
+                if (this.status == 200)
+                {
                     check_version(
                         checked_url_,
                         lower_version_index_,
@@ -100,7 +135,8 @@ function check_version(checked_url_, lower_version_index_, upper_version_index_,
                         checked_path_
                     );
                 }
-                else if (this.status == 404) {
+                else if (this.status == 404)
+                {
                     check_version(
                         checked_url_,
                         current_version_index+1,
@@ -111,17 +147,33 @@ function check_version(checked_url_, lower_version_index_, upper_version_index_,
             }
         };
 
-        xhttp.open('GET', checked_url_ + checked_version + checked_path_, true);
+        xhttp.open(
+            'GET',
+            checked_url_ + checked_version + checked_path_,
+            true
+        );
         xhttp.send();
     }
 }
 
-function check_old_version(checked_url_, lower_version_index_, upper_version_index_, checked_feature_) {
-    if (lower_version_index_ > upper_version_index_) {
+function check_old_version(
+    checked_url_,
+    lower_version_index_,
+    upper_version_index_,
+    checked_feature_
+)
+{
+    if (lower_version_index_ > upper_version_index_)
+    {
         show_since(cmake_old_versions[lower_version_index_]);
     }
-    else {
-        var current_version_index = Math.floor(lower_version_index_ + (upper_version_index_ - lower_version_index_)/2);
+    else
+    {
+        var current_version_index = Math.floor(
+            lower_version_index_ + (
+                upper_version_index_ - lower_version_index_
+            )/2
+        );
 
         var checked_version = cmake_old_versions[current_version_index];
 
@@ -129,13 +181,17 @@ function check_old_version(checked_url_, lower_version_index_, upper_version_ind
 
         var to_open = checked_url_ + "cmake2.6docs.html";
 
-        if (current_version_index >= cmake_old_versions.indexOf("v2.8.0")) {
+        if (current_version_index >= cmake_old_versions.indexOf("v2.8.0"))
+        {
             to_open = checked_url_ + checked_version + "/cmake.html";
         }
 
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                if (this.responseText.includes(checked_feature_)) {
+        xhttp.onreadystatechange = function()
+        {
+            if (this.readyState == 4 && this.status == 200)
+            {
+                if (this.responseText.includes(checked_feature_))
+                {
                     check_old_version(
                         checked_url_,
                         lower_version_index_,
@@ -143,7 +199,8 @@ function check_old_version(checked_url_, lower_version_index_, upper_version_ind
                         checked_feature_
                     );
                 }
-                else if (current_version_index < cmake_old_versions.length-1) {
+                else if (current_version_index < cmake_old_versions.length-1)
+                {
                     check_old_version(
                         checked_url_,
                         current_version_index+1,
@@ -151,7 +208,8 @@ function check_old_version(checked_url_, lower_version_index_, upper_version_ind
                         checked_feature_
                     );
                 }
-                else {
+                else
+                {
                     // the feature doesn't exist in old versions
                     show_since(cmake_versions[0]);
                 }
@@ -163,11 +221,14 @@ function check_old_version(checked_url_, lower_version_index_, upper_version_ind
     }
 }
 
-function show_since(since_version_) {
+function show_since(since_version_)
+{
     var csince_elements = document.getElementsByClassName('csince');
     var i;
 
-    for (i = 0; i < csince_elements.length; ++i) {
-        csince_elements[i].innerHTML = '(since CMake ' + since_version_ + ')';
+    for (i = 0; i < csince_elements.length; ++i)
+    {
+        csince_elements[i].textContent =
+            '(since CMake ' + since_version_ + ')';
     }
 }
